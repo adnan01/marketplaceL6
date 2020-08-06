a24333bbf95e43593509bc9272fd8a7b0d510bcc
Adnan A Araujo <adnanaraujo10@gmail.com>
2020-08-06 15:03:52 -0400

    recuperando dados no formStore após erro nos campos

--- resources/views/admin/stores/create.blade.php
+++ resources/views/admin/stores/create.blade.php
@@ -6,7 +6,7 @@
 <input type="hidden" name="_token" value="{{csrf_token()}}">
 <div class="form-group">
     <label for="">Nome da loja</label>
-    <input type="text" name="name" class="form-control @error('name') is-invalid @enderror">
+    <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" value="{{old('name')}}">
 
     @error('name')
         <div class="invalid-feedback">
@@ -16,33 +16,33 @@
 </div>
 <div class="form-group">
     <label for="">Descrição</label>
-    <input type="text" name="description" id="" class="form-control @error('description') is-invalid @enderror">
+    <input type="text" name="description" id="" class="form-control @error('description') is-invalid @enderror" value="{{old('description')}}">
 
     @error('description')
-    <div class="invalid-feedback">
-        {{$message}}
-    </div>
-@enderror
+        <div class="invalid-feedback">
+            {{$message}}
+        </div>
+    @enderror
 </div>
 <div class="form-group">
     <label for="">Telefone</label>
-    <input type="text" name="phone" id="" class="form-control @error('phone') is-invalid @enderror">
+    <input type="text" name="phone" id="" class="form-control @error('phone') is-invalid @enderror" value="{{old('phone')}}">
 
     @error('phone')
-    <div class="invalid-feedback">
-        {{$message}}
-    </div>
-@enderror
+        <div class="invalid-feedback">
+            {{$message}}
+        </div>
+    @enderror
 </div>
 <div class="form-group">
     <label for="">Celular/Whatsapp</label>
-    <input type="text" name="mobile_phone" id="" class="form-control @error('mobile_phone') is-invalid @enderror">
+    <input type="text" name="mobile_phone" id="" class="form-control @error('mobile_phone') is-invalid @enderror" value="{{old('mobile_phone')}}">
 
     @error('mobile_phone')
-    <div class="invalid-feedback">
-        {{$message}}
-    </div>
-@enderror
+        <div class="invalid-feedback">
+            {{$message}}
+        </div>
+    @enderror
 </div>
 <div class="form-group">
     <label for="">Slug</label>
